d7d2505038ce3ff063133bb40d63c6dc678d6c28
Jun-KR <jundevkr@gmail.com>
2021-03-14 17:31:26 +0900

    Initial commit

---
+++ NoXbox.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @name NoXbox
+ * @main JUNKR\NoXbox
+ * @author JUN-KR
+ * @version 1.0.0
+ * @api 3.14.0
+ * @description -
+ */
+
+namespace JUNKR;
+
+use pocketmine\entity\Skin;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\network\mcpe\protocol\PlayerListPacket;
+use pocketmine\network\mcpe\protocol\types\PlayerListEntry;
+use pocketmine\network\mcpe\protocol\types\SkinAdapterSingleton;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+class NoXbox extends PluginBase implements Listener{
+
+    private $pk = [];
+
+    public function onEnable(){
+        $this->getserver()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onJoin(PlayerJoinEvent $ev){
+        $player = $ev->getPlayer();
+
+        $add = new PlayerListPacket();
+        $add->type = PlayerListPacket::TYPE_REMOVE;
+        $add->entries = [PlayerListEntry::createRemovalEntry($player->getUniqueId())];
+        $this->pk[] = $add;
+
+        $add->type = PlayerListPacket::TYPE_ADD;
+        $add->entries = [PlayerListEntry::createAdditionEntry($player->getUniqueId(), $player->getId(), $player->getName(), SkinAdapterSingleton::get()->toSkinData($player->getSkin()))];
+        $this->pk[] = $add;
+
+        foreach($this->pk as $pk){
+            Server::getInstance()->broadcastPacket(Server::getInstance()->getOnlinePlayers(), $pk);
+        }
+    }
+
+}
