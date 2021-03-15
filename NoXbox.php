c15b46ef82bf926362bd27b4072d0ceacaed9962
Jun-KR <jundevkr@gmail.com>
2021-03-15 12:07:26 +0900

    나가있어도 오른쪽에 표시되는 버그 해결

--- NoXbox.php
+++ NoXbox.php
@@ -22,26 +22,26 @@ use pocketmine\Server;
 
 class NoXbox extends PluginBase implements Listener{
 
-    private $pk = [];
-
     public function onEnable(){
         $this->getserver()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onJoin(PlayerJoinEvent $ev){
-        $player = $ev->getPlayer();
+        $pk = [];
 
-        $add = new PlayerListPacket();
-        $add->type = PlayerListPacket::TYPE_REMOVE;
-        $add->entries = [PlayerListEntry::createRemovalEntry($player->getUniqueId())];
-        $this->pk[] = $add;
+        foreach(Server::getInstance()->getOnlinePlayers() as $player){
+            $add = new PlayerListPacket();
+            $add->type = PlayerListPacket::TYPE_REMOVE;
+            $add->entries = [PlayerListEntry::createRemovalEntry($player->getUniqueId())];
+            $pk[] = $add;
 
-        $add->type = PlayerListPacket::TYPE_ADD;
-        $add->entries = [PlayerListEntry::createAdditionEntry($player->getUniqueId(), $player->getId(), $player->getDisplayName(), SkinAdapterSingleton::get()->toSkinData($player->getSkin()))];
-        $this->pk[] = $add;
+            $add->type = PlayerListPacket::TYPE_ADD;
+            $add->entries = [PlayerListEntry::createAdditionEntry($player->getUniqueId(), $player->getId(), $player->getName(), SkinAdapterSingleton::get()->toSkinData($player->getSkin()))];
+            $pk[] = $add;
+        }
 
-        foreach($this->pk as $pk){
-            Server::getInstance()->broadcastPacket(Server::getInstance()->getOnlinePlayers(), $pk);
+        foreach($pk as $pks){
+            Server::getInstance()->broadcastPacket(Server::getInstance()->getOnlinePlayers(), $pks);
         }
     }
 
